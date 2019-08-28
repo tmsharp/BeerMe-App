4b02a3ef3e229eb88a991493f2c228623fcd993b
tmsharp <34277371+tmsharp@users.noreply.github.com>
2019-08-28 11:17:49 -0400

    wip

--- app.py
+++ app.py
@@ -30,6 +30,8 @@ import numpy as np
 
 from util import *
 
+from tabs import me
+
 # Layout
 app.layout = html.Div([
 
@@ -43,135 +45,45 @@ app.layout = html.Div([
     # url
     dcc.Location(id='url', refresh=False),
 
-   html.Div(className = 'container my-4', children =[
-    #    # username section
-    #    html.Div(className='card', children=[
-    #        html.Div(className='col-lg-5 m-4', children=[
-    #             dcc.Dropdown(
-    #                 id = 'username-selection-dropdown',
-    #                 options=username_options(),
-    #                 multi = False
-    #             )
-    #        ]),
-    #     ]),
-        # search section
-        html.Div(id='search-section', className='card', children = [
-            html.Div(className='card-body', children = [
-                html.H2(className='card-title text-center', children = "Find My Beer"),
-                html.Div(className='card-text text-center', children = [
-                        """
-                        Select up to 5 beers and our algorithm will predict which beer you should drink!
-                        """
-                ]),
-                html.Div(className='row justify-content-center', children=[
-                    html.Div(className='col-lg-5 m-4', children=[
-                        dcc.Dropdown(
-                            id = 'beer-selection-dropdown',
-                            options = beer_options(),
-                            multi = True
-                        )
-                    ]),
-                ]),
-                html.Div(className='row justify-content-center', children=[
-                    html.Button('Search', id='search-button', className='btn btn-outline-primary')
-                ]),
-                 html.Div(className='row justify-content-center my-3', children=[
-                    html.Div(id='search-results')
-                ]),
-                html.Div(className='row justify-content-center my-3', children=[
-                    html.Img(id='beer-loader', src='/assets/img/beer-loader.gif'),
-                ]),
-            ]),
-        ]),
+    # tabs
+    dcc.Tabs(id="tabs", value='me', children=[
+        dcc.Tab(label='Me', value='me'),
+        dcc.Tab(label='Content Based Filtering', value='cbf'),
     ]),
-    # end container
+
+    # page content
+    html.Div(id='page-content')
 
 ])
 
-## callbacks
-@app.callback(Output('beer-loader', 'style'),
-                [Input('search-button', 'n_clicks')],
-                [State('beer-selection-dropdown', 'value')])
-def display_beer_loader(n_clicks, value):
-    if value != None:
-        if len(value) == 1:
-            return {'display': 'none'}
-    elif n_clicks != None and value != None and value != []:
-        time.sleep(0.25)
-        return {'display': 'block'}
-    else:
-        return {'display': 'none'}
 
-@app.callback(Output('search-results', 'children'),
-                [Input('search-button', 'n_clicks')],
-                [State('beer-selection-dropdown', 'value')])
-def display_beer_loader(n_clicks, value):
+### callbacks for page content and url 
+# output url for clicking tabs
+@app.callback(Output('url', 'pathname'),
+              [Input('tabs','value')])
+def tab_selection(tab):
+   '''
+   @doc: callback function for the page buttons in the header
+   @args: pathname is a str representing the button path that points to the
+      correct html.Div() objects defined above.
+   @return: the appropriate html.Div() object as defined above
+   '''
+   if tab == 'me':
+      return '/me'
+   elif tab == 'cbf':
+      return '/cbf'
+   else:
+      return '/'
 
-    if value != None:
-        if len(value) == 1:
-            return "Please select more than one beer!"
 
-    if n_clicks != None and value != None and value != []:
-        
-        features = ['ABV', 'IBU', 'global_rating']
-
-        query = """SELECT * FROM user_extract 
-                    WHERE beer_name IN {}""".format(tuple(value))
-
-        df = import_table('data/beer.db', query=query)
-        df = df[~df.duplicated()]
-        df = impute_na(df, features=features)
-
-        # convert IBU from str to float (str due to impute)
-        if 'IBU' in features:
-            df['IBU'] = df['IBU'].astype(float)
-
-        # take mean of global rating since it changes based on when users data is scraped
-        if 'global_rating' in features:
-            df = df.groupby('beer_name').mean()
-        
-        df = df[~df.duplicated()]
-
-        # scale features
-        with open('models/X_scaler-final_model.pkl', 'rb') as file:
-            X_scaler = pickle.load(file)
-        with open('models/y_scaler-final_model.pkl', 'rb') as file:
-            y_scaler = pickle.load(file)
-        X_scaler.fit(df[features])
-        df[features] = X_scaler.transform(df[features])
-        
-        # import model and predict
-        with open('models/final_model.pkl', 'rb') as file:
-            model = pickle.load(file)
-        preds = model.predict(df[features])
-
-        # inverse scale and rebuild df
-        predictions = y_scaler.inverse_transform(preds)
-        df[features] = X_scaler.inverse_transform(df[features])
-        df['predictions'] = predictions
-
-        # rename global rating
-        df.rename({'gloabl_rating': 'average_global_rating'}, inplace=True, axis=1)
-
-        # sort by original order
-        df = df.loc[value]
-
-        # sort by predictions
-        df = df.sort_values('predictions', ascending=False)
-
-        random_responses = ["Our best guess is you're gonna love {}", 
-                            "Have a cold {} for us!", 
-                            "Don't even think about any other option, {} is your next cold one!"]
-        rand_ind = np.random.randint(0,len(random_responses))
-
-        return random_responses[rand_ind].format(str(df.index[0]))
-    
-    elif n_clicks != None:
-        return ":( Please select at least one beer"
-
-    else:
+# display each app's content based on url (which is updated by tab clicks)
+@app.callback(Output('page-content', 'children'),
+              [Input('url', 'pathname')])
+def display_page(pathname):
+    if pathname == '/cbf':
         pass
-
+    else:
+        return me.layout
 
 
 ## run
